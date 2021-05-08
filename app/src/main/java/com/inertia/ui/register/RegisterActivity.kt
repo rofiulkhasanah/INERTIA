8d3fb5be4dd6ae06d99b4c54df1464f8c36c89c9
rofiulkhasanah <rofiulkhasanah01@gmail.com>
2021-05-08 14:40:29 +0700

    ui register

---
+++ app/src/main/java/com/inertia/ui/register/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.inertia.ui.register
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.inertia.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
