b2f7d3e167bb0b91ff983d8397738d7c9a759e33
Irfan Rafif <mirfanrafif17@gmail.com>
2021-06-05 09:13:33 +0700

    bersih2 juga

--- app/src/main/java/com/inertia/utils/MultipartHelper.kt
+++ app/src/main/java/com/inertia/utils/MultipartHelper.kt
@@ -3,10 +3,15 @@ package com.inertia.utils
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
 
 object MultipartHelper {
     fun getPart(file: File): MultipartBody.Part {
-        return MultipartBody.Part.createFormData("foto", file.name, RequestBody.create("image/*".toMediaTypeOrNull(), file))
+        return MultipartBody.Part.createFormData(
+            "foto",
+            file.name,
+            file.asRequestBody("image/*".toMediaTypeOrNull())
+        )
     }
 }
